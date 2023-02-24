683d1da2a566ff2400a538053bed15db3adb31d8
˹Sʜɪᴠᴀᴍ ꭙ Jᴀɪsᴡᴀʟ˼❤️ <111850575+Shivam892483@users.noreply.github.com>
2023-02-24 14:28:45 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE Fallen Robot
+TITLE Angel Robot
 :: Enables virtual env mode and then starts Angel
-env\scripts\activate.bat && py -m FallenRobot
+env\scripts\activate.bat && py -m Angelrobot
